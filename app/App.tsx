4cb307a31109be18891632e7c329e901f5b2311d
pedronorton <p.paiva.pdo@gmail.com>
2022-10-05 23:53:49 -0300

    implementa base para login

--- app/App.tsx
+++ app/App.tsx
@@ -3,16 +3,42 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import QRCodeScreen from './src/screens/QRCode';
+import Login from './src/screens/Login';
 import ScanScreen from './src/screens/Scan';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
 
 const Stack = createStackNavigator();
 
 const Application = () => {
+    const [auth, setAuth] = useState(false);
+
+    useEffect(() => {
+        fetchStorageData();
+    }, []);
+
+    const fetchStorageData = async () => {
+        try {
+            const storageToken = await AsyncStorage.getItem('@token');
+            const storageMail = await AsyncStorage.getItem('@userMail');
+            console.log('fetch');
+
+            if (storageToken !== null && storageMail != null) {
+                console.log('entrou');
+            } else {
+                setAuth(true);
+                console.log('erro ao logar');
+            }
+        } catch (e) {
+            console.log('erro ao logar');
+        }
+    };
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="QRCode">
+            <Stack.Navigator initialRouteName={auth == true ? 'QRCode' : 'Login'}>
                 <Stack.Screen name={'QRCode'} component={QRCodeScreen} />
                 <Stack.Screen name={'Scan'} component={ScanScreen} />
+                <Stack.Screen name={'Login'} component={Login} />
             </Stack.Navigator>
             <StatusBar style="auto" />
         </NavigationContainer>
--- app/src/service/DataService.js
+++ app/src/service/DataService.js
@@ -1,10 +1,13 @@
 import Axios from 'axios';
 
-const URL = 'http://192.168.0.101:8080/api';
+const URL = 'http://192.168.0.108:8080/api';
 
 class DataService {
     postPresence(url) {
         return Axios.get(url);
+    }
+    login(data) {
+        return Axios.post(`${URL}/auth/signin`, data);
     }
 }
 
